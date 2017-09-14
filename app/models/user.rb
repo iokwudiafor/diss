35420db27ea518d072931227ea3642df18f1fd48
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 05:09:35 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. 
+  # Include default devise modules and the user has a "profile". 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_one :profile
