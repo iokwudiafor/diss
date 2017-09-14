dc621ab99609ec825bb815b30454735528a67ef7
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 05:17:55 +0100

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
+#if an email is sent out from the app use 'support@soundzgood.com' as the email and use the mailer layout in the views folder
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'support@soundzgood.com'
   layout 'mailer'
 end
