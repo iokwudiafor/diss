bad1c7f1f14b895502662b318e558fc4f4a184bb
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 05:26:27 +0100

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,16 @@
+#Create profile and assignit a user id requiring first name last name favorite artist and bio
 class ProfilesController < ApplicationController
   
   def new
     @profile = Profile.new
   end
-
     def create
       # Make sure user is filling out form
       @user = User.find( params[:user_id] )
       # Profile linked to user
       @profile = @user.build_profile( profile_params )
       if @profile.save
-        flash[:success] = "Profile lit!"
+        flash[:success] = "Profile Lit!"
         redirect_to user_path( params[:user_id] )
       else
         render action: :new
