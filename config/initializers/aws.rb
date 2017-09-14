e9848d6030bcb38ab0782f385884c5e420314b63
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 03:07:28 +0100

    Delete aws.rb

--- config/initializers/aws.rb
+++
@@ -1,9 +0,0 @@
-#require 'aws-sdk'
-#require'aws-sdk-s3'
-
-#Aws.config.update( region: 'us-west-2', credentials: Aws::Credentials.new('AKIAIKZYAKUIN23FTMKQ', '16iBKOsmrvr6LzOoEcnRNyWalJnbCBhGAQkMQS1D')){
-
-#s3 = Aws::S3::Client.new
-
-
-#}
