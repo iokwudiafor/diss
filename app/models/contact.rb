dd996bbb6a964648f7bef89b8d6632fb15a74819
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 05:11:02 +0100

    Update contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
+#In the contact form make sure there fields name, email, and comments are present
 class Contact < ActiveRecord::Base
     validates :name, presence: true
     validates :email, presence: true
