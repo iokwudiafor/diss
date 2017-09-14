aae5cb1c105d6e7be1ca87ae21d463475903b947
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 15:48:00 +0100

    Update 20170815082621_devise_create_users.rb

--- db/migrate/20170815082621_devise_create_users.rb
+++ db/migrate/20170815082621_devise_create_users.rb
@@ -1,4 +1,4 @@
-#Migration file for user profiles
+#Migration file for user profilesset up by devise gem
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
