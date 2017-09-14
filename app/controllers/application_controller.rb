a8804b31507dcbc6cdfb0c4f9ebef432eff5f38e
Ikem Okwudiafor <iokwudiafor@gmail.com>
2017-09-14 05:59:55 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+#security measurement taken by default in Ruby to mitigate multiple requests from the same client
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
